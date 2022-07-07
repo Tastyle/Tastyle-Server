90558158ecc661053011cc74c271892973a5a16b
Jeong-w-o-o <irmnels@gmail.com>
2022-07-08 00:12:59 +0900

    [TASTYLE-134] :tada: Initial Commit

---
+++ src/test/java/org/tastyle/tastyle/TastyleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tastyle.tastyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

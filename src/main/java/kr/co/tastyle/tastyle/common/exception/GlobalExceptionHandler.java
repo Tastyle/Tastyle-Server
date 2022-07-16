df19ae2b26d160051934742a8bacb404cc4478d9
Jeong-w-o-o <irmnels@gmail.com>
2022-07-16 23:55:05 +0900

    [TASTYLE-142] Feat: Validation Exception 추가

--- src/main/java/kr/co/tastyle/tastyle/common/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/co/tastyle/tastyle/common/exception/GlobalExceptionHandler.java
@@ -2,9 +2,15 @@ package kr.co.tastyle.tastyle.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -19,6 +25,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> maxUploadSizeExceededException(MaxUploadSizeExceededException e) {
         log.error("Handle maxUploadSizeExceededException: {}", e.getMessage());
         return ErrorResponse.toFileUploadExceptionEntity(e);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> validationException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        Map<String, String> errors = new HashMap<>();
+        bindingResult.getAllErrors()
+                .forEach(c -> errors.put(((FieldError) c).getField(), c.getDefaultMessage()));
+        return ErrorResponse.toValidationException(errors);
     }
 
     @ExceptionHandler(Exception.class)

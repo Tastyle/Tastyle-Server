7e11854bac9b6e4064a50b04f98c7912a6874dc0
Jeong-w-o-o <irmnels@gmail.com>
2022-07-18 23:48:11 +0900

    [TST-163] 응답에 ResponseCode  상태, 결과 추가

--- src/main/java/kr/co/tastyle/tastyle/common/response/CommonResponse.java
+++ src/main/java/kr/co/tastyle/tastyle/common/response/CommonResponse.java
@@ -2,26 +2,29 @@ package kr.co.tastyle.tastyle.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import static kr.co.tastyle.tastyle.common.response.ResponseCode.SUCCESS;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class CommonResponse<T> {
-    private int status;
+    private int code;
     private String message;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T result;
 
     public CommonResponse(T result) {
-        this.status = SUCCESS.getHttpStatus().value();
+        this.code = SUCCESS.getCode();
         this.message = SUCCESS.getMessage();
         this.result = result;
     }
 
-    public CommonResponse(ResponseCode status) {
-        this.status = SUCCESS.getHttpStatus().value();
+    public CommonResponse(ResponseCode status, T result) {
+        this.code = status.getCode();
         this.message = status.getMessage();
+        this.result = result;
     }
 }

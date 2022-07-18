ccd7efe552e6b79085100ccc134f4a939044a9a8
Jeong-w-o-o <irmnels@gmail.com>
2022-07-18 23:48:44 +0900

    [TST-163] CREATED 코드 추가

--- src/main/java/kr/co/tastyle/tastyle/common/response/ResponseCode.java
+++ src/main/java/kr/co/tastyle/tastyle/common/response/ResponseCode.java
@@ -1,18 +1,16 @@
 package kr.co.tastyle.tastyle.common.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@AllArgsConstructor
 public enum ResponseCode {
-    SUCCESS(HttpStatus.OK, "요청에 성공하였습니다.")
+    SUCCESS(1000, "요청에 성공하였습니다."),
+    CREATED(1001,"생성에 성공하였습니다."),
     ;
 
-    private HttpStatus httpStatus;
+    private int code;
     private String message;
-
-    ResponseCode(HttpStatus httpStatus, String message) {
-        this.httpStatus = httpStatus;
-        this.message = message;
-    }
 }

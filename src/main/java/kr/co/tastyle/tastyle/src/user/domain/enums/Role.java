2aaa22c98e223550beebba4d5320d9963d868de1
Jeong-w-o-o <irmnels@gmail.com>
2022-07-20 01:53:04 +0900

    [TST-168] 유저 enum 타입 추가

---
+++ src/main/java/kr/co/tastyle/tastyle/src/user/domain/enums/Role.java
@@ -0,0 +1,14 @@
+package kr.co.tastyle.tastyle.src.user.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+    ROLE_USER("일반 유저"),
+    ROLE_ADMIN("관리자"),
+    ;
+
+    private final String description;
+}

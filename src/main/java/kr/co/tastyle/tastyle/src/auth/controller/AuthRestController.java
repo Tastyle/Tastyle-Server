b0fe4e800589046e6ba0ecb790ec9b40de13001d
Jeong-w-o-o <irmnels@gmail.com>
2022-07-22 02:42:33 +0900

    [TST-17] Feat: 회원가입, 로그인 시 상태코드 다르게 추가

--- src/main/java/kr/co/tastyle/tastyle/src/auth/controller/AuthRestController.java
+++ src/main/java/kr/co/tastyle/tastyle/src/auth/controller/AuthRestController.java
@@ -8,6 +8,7 @@ import kr.co.tastyle.tastyle.src.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.util.Pair;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -18,11 +19,11 @@ public class AuthRestController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public CommonResponse<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<CommonResponse<LoginResponse>> login(@RequestBody LoginRequest loginRequest) {
         log.info("[AuthRestController] login");
         Pair<ResponseCode, LoginResponse> response = authService.login(loginRequest);
         ResponseCode responseCode = response.getFirst();
         LoginResponse loginResponse = response.getSecond();
-        return new CommonResponse<>(responseCode, loginResponse);
+        return ResponseEntity.status(responseCode.getHttpStatus()).body(new CommonResponse<>(loginResponse));
     }
 }

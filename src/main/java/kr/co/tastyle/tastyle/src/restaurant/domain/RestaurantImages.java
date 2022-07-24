e6a83dcccc92674130ee4a5639a13b74f4955a05
Jeong-w-o-o <irmnels@gmail.com>
2022-07-24 22:19:32 +0900

    [TST-181] Feat: 음식점 이미지 도메인 추가

---
+++ src/main/java/kr/co/tastyle/tastyle/src/restaurant/domain/RestaurantImages.java
@@ -0,0 +1,25 @@
+package kr.co.tastyle.tastyle.src.restaurant.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Builder
+@Table(name = "restaurant_images")
+public class RestaurantImages {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    private String restaurantImageUrl;
+
+    private int imageOrder;
+}

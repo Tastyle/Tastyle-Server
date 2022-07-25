570661793d61f7dbd1c9a2d87278298d94a1c67b
Jeong-w-o-o <irmnels@gmail.com>
2022-07-25 22:46:47 +0900

    Refactor: 익셉션에 에러 코드 import static 삭제

--- src/main/java/kr/co/tastyle/tastyle/jwt/CustomUserDetailsService.java
+++ src/main/java/kr/co/tastyle/tastyle/jwt/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package kr.co.tastyle.tastyle.jwt;
 
 import kr.co.tastyle.tastyle.common.exception.CommonException;
+import kr.co.tastyle.tastyle.common.exception.ErrorCode;
 import kr.co.tastyle.tastyle.src.user.domain.User;
 import kr.co.tastyle.tastyle.src.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,8 +10,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import static kr.co.tastyle.tastyle.common.exception.ErrorCode.NOT_FOUND_MEMBER;
 
 @Service
 @Transactional(readOnly = true)
@@ -21,7 +20,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String socialId) throws UsernameNotFoundException {
         User user = userRepository.findBySocialId(socialId)
-                .orElseThrow(() -> new CommonException(NOT_FOUND_MEMBER));
+                .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
         return SecurityUser.of(user);
     }
 }

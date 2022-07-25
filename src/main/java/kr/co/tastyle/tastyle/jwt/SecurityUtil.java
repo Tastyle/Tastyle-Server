b905bb444548dcfd1219cc19d46a74dc4ca909cf
Jeong-w-o-o <irmnels@gmail.com>
2022-07-25 22:47:28 +0900

    Feat: User 반환하는 getUser 메소드 추가

--- src/main/java/kr/co/tastyle/tastyle/jwt/SecurityUtil.java
+++ src/main/java/kr/co/tastyle/tastyle/jwt/SecurityUtil.java
@@ -1,5 +1,6 @@
 package kr.co.tastyle.tastyle.jwt;
 
+import kr.co.tastyle.tastyle.src.user.domain.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -8,5 +9,11 @@ public class SecurityUtil {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         SecurityUser securityUser = (SecurityUser) authentication.getPrincipal();
         return securityUser.getUserId();
+    }
+
+    public static User getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityUser securityUser = (SecurityUser) authentication.getPrincipal();
+        return securityUser.getUser();
     }
 }

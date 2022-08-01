54b3534d8dd0849dd304e9d27f0998ba263186a4
Jeong-w-o-o <irmnels@gmail.com>
2022-08-01 23:52:07 +0900

    Feat: 에러 메시지 변경

--- src/main/java/kr/co/tastyle/tastyle/src/auth/client/ClientGoogle.java
+++ src/main/java/kr/co/tastyle/tastyle/src/auth/client/ClientGoogle.java
@@ -1,15 +1,13 @@
 package kr.co.tastyle.tastyle.src.auth.client;
 
 import kr.co.tastyle.tastyle.common.exception.CommonException;
+import kr.co.tastyle.tastyle.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import static kr.co.tastyle.tastyle.common.exception.ErrorCode.INVALID_KAKAO_SERVER_ERROR;
-import static kr.co.tastyle.tastyle.common.exception.ErrorCode.INVALID_KAKAO_TOKEN;
 
 @Component
 @RequiredArgsConstructor
@@ -22,8 +20,8 @@ public class ClientGoogle implements ClientProxy {
         JSONObject response = webClient.get()
                 .uri(GOOGLE_URL, builder -> builder.queryParam("id_token", accessToken).build())
                 .retrieve()
-                .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new CommonException(INVALID_KAKAO_TOKEN)))
-                .onStatus(HttpStatus::is5xxServerError, serverResponse -> Mono.error(new CommonException(INVALID_KAKAO_SERVER_ERROR)))
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new CommonException(ErrorCode.INVALID_OAUTH_TOKEN)))
+                .onStatus(HttpStatus::is5xxServerError, serverResponse -> Mono.error(new CommonException(ErrorCode.INVALID_OAUTH_SERVER_ERROR)))
                 .bodyToMono(JSONObject.class)
                 .block();
 
